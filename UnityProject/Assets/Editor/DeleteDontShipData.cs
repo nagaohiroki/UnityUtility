cc83dbc1698df30f0ec15b2c848075d62456e7ac
nagaohiroki <testplayers0226@gmail.com>
2023-02-01 11:21:10 +0900

    add DeleteDontShipData

---
+++ UnityProject/Assets/Editor/DeleteDontShipData.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+public class DeleteDontShipData
+{
+	[PostProcessBuild(2000)]
+	public static void OnPostprocessBuild(BuildTarget target, string path)
+	{
+		if(EditorUserBuildSettings.development)
+		{
+			return;
+		}
+		var keyWords = new[]
+		{
+			"_BackUpThisFolder_ButDontShipItWithYourGame",
+			"_BurstDebugInformation_DoNotShip"
+		};
+		var dirs = Directory.GetDirectories(Path.GetDirectoryName(path));
+		foreach(var dir in dirs)
+		{
+			foreach(var key in keyWords)
+			{
+				if(dir.Contains(key))
+				{
+					Directory.Delete(dir, true);
+					break;
+				}
+			}
+		}
+	}
+}
